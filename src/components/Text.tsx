c1f1572a70eff17460eabd94a191b999e28fda3f
Tomasz Stanisz <fremen1990@gmail.com>
2023-01-20 21:30:43 +0100

    Section 4: Build a reusable polymorphic component utility

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,19 +3,43 @@ import {ComponentPropsWithoutRef, ElementType, PropsWithChildren, ReactNode} fro
 // rainbow ROYGBIV
 type Rainbow = 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'indigo' | 'violet'
 
-type TextProps<C extends ElementType> = {
-    as?: C;
-    color?: Rainbow | "black";
-    // ==== PropsWithChildren====
-    // children: ReactNode
+
+type AsProp<C extends ElementType> = {
+    as?: C
 }
 
+// type TextProps<C extends ElementType> = AsProp<C> & {
+//     color?: Rainbow | "black";
+//     // ==== PropsWithChildren====
+//     // children: ReactNode
+// }
 
-type Props<C extends ElementType> =
-    PropsWithChildren<TextProps<C>>
-    & Omit<ComponentPropsWithoutRef<C>, keyof TextProps<C>>
 
-export const Text = <C extends ElementType = 'span'>({as, color, style, children, ...restProps}: Props<C>) => {
+// type Props<C extends ElementType> =
+//     PropsWithChildren<TextProps<C>>
+//     & Omit<ComponentPropsWithoutRef<C>, keyof TextProps<C>>
+
+
+type PropsToOmit<C extends ElementType, P> = keyof (AsProp<C> & P)
+
+type PolymorphicComponentProps<C extends ElementType, Props = {}> =
+    PropsWithChildren<Props & AsProp<C>>
+    & Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>
+
+
+type TextProps = {
+    color?: Rainbow | "black";
+}
+
+// TOD: 20. The problem(s) we want to tackle
+
+export const Text = <C extends ElementType = 'span'>({
+                                                         as,
+                                                         color,
+                                                         style,
+                                                         children,
+                                                         ...restProps
+                                                     }: PolymorphicComponentProps<C, TextProps>) => {
 
     const Component = as || 'span'
 
@@ -25,5 +49,6 @@ export const Text = <C extends ElementType = 'span'>({as, color, style, children
         <Component {...restProps} {...internalStyles}>{children}</Component>
     )
 };
+
 
 // <Text as="">Hello World</Text>
